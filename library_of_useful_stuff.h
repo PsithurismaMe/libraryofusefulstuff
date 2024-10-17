6095abd218deec169f1a236638ea18bbe129597c
PsithurismaMe <psithurisma.me@gmail.com>
2024-10-17 10:42:07 -0700

    fixed a typo

--- library_of_useful_stuff.h
+++ library_of_useful_stuff.h
@@ -8,7 +8,7 @@ int universal_get_char();
 // This function spams a bunch of newlines to stdout, "clearing" the console.
 void clear_console();
 
-// Does the same thing as strbncmp except for integer arrays.
+// Does the same thing as strncmp except for integer arrays.
 int int_array_cmp(int * a, int * b, size_t size);
 
 #endif
